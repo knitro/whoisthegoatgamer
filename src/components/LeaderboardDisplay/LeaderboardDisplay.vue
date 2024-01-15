aec03ca8b7b0eef9439f2381e899f8ea10e0c1a1
Calvin Lee <calvin.hanbee.lee@gmail.com>
2024-01-15 18:18:16 +1300

    Add colours and icons to leaderboard

--- src/components/LeaderboardDisplay/LeaderboardDisplay.vue
+++ src/components/LeaderboardDisplay/LeaderboardDisplay.vue
@@ -2,9 +2,12 @@
   <v-card title="Leaderboard" min-height="100%" min-width="100%">
     <v-list>
       <v-list-item
-        v-for="scoreDisplay in scoreDisplayArray"
+        v-for="(scoreDisplay, index) in scoreDisplayArray"
         v-bind:key="'score_' + scoreDisplay.playerId"
       >
+        <template v-slot:prepend>
+          <v-icon :icon="getIcon(index)" :color="getColour(index)"></v-icon>
+        </template>
         <template v-slot:append>
           {{ scoreDisplay.score }}
         </template>
@@ -25,6 +28,32 @@ const props = defineProps({
     required: true,
   },
 });
+
+function getIcon(index: number) {
+  switch (index) {
+    case 0:
+      return "mdi-trophy";
+    case 1:
+      return "mdi-medal";
+    case 2:
+      return "mdi-medal-outline";
+    default:
+      return "mdi-account";
+  }
+}
+
+function getColour(index: number) {
+  switch (index) {
+    case 0:
+      return "#FFD700";
+    case 1:
+      return "#C0C0C0";
+    case 2:
+      return "#CD7F32";
+    default:
+      return "#000000";
+  }
+}
 </script>
 
 <style scoped lang="scss"></style>
